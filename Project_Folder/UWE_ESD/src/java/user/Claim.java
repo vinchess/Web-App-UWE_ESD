070ffd25ab4b2cec621f5a2fd0ebb8bbe3023c85
User <User@100.74.245.115>
2016-11-14 15:17:12 +0000

    created claim java class, declared variables, getmethod()

---
+++ Project_Folder/UWE_ESD/src/java/user/Claim.java
@@ -0,0 +1,32 @@
+package user;
+
+import package1.User;
+
+/**
+ *
+ * @author Jayson
+ */
+public class Claim
+{
+  private String m_name;
+
+  private String m_claimType;
+
+  private Long  m_claimNumber;
+  
+  User user = new User();
+  
+  public String getClaimType()
+  {
+    return m_claimType;
+  }
+
+  public Long getClaimNumber()
+  {
+    return m_claimNumber;
+  }
+}
+
+
+    
+
