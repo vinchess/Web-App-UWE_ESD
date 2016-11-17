86cfca06b59e6243e7b10188b4ceeabe9d15f567
Vince <vincentlim.spannar@gmail.com>
2016-11-17 09:59:37 +0000

    Fixed payment update to sql executeBatch

--- Project_Folder/UWE_ESD/src/java/database/PaymentDAO.java
+++ Project_Folder/UWE_ESD/src/java/database/PaymentDAO.java
@@ -8,23 +8,36 @@ import user.User;
 public class PaymentDAO extends JDBC{
     public boolean updatePayment(User user, double payment){
         boolean result = false;
-        //this line
-        String sql = /*"UPDATE members SET balance=balance+"+ payment +
-                            " WHERE id='"+ user.getID() + "';\n" +*/
-                     "INSERT INTO payments(mem_id,type_of_payment,amount,date) " +
-                            "VALUES('" + user.getID() + "','PAYMENT'," + payment + ",now());";
         
         conn = getConnection();
         
         try{
             stmt = conn.createStatement();
+            conn.setAutoCommit(false);
 
-            int updateResult = stmt.executeUpdate(sql);
-            if (updateResult==1) result = true;
+            stmt.addBatch("UPDATE members SET balance=balance+"+ payment +
+                            " WHERE id='"+ user.getID() + "';");
+            stmt.addBatch("INSERT INTO payments(mem_id,type_of_payment,amount,date) " +
+                            "VALUES('" + user.getID() + "','PAYMENT'," + payment + ",now());");
+            
+            int[] updateResult = stmt.executeBatch();
+            
+            if(checkResult(updateResult)){
+                conn.commit();
+            }else{
+                conn.close();
+                throw new SQLException("Commit Failed");
+            }
+            
             conn.close();
         }catch(SQLException se){
-            System.out.println("SQL error occurred.");
+            System.out.println("SQL error occurred. " + se.getMessage());
         }
         return result;
+    }
+    
+    private boolean checkResult(int[] updateResults){
+        for(int i : updateResults) if(i!=1) return false;
+        return true;
     }
 }
