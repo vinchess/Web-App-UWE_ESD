20e6e8460e2a559458d21ff7fda21e0252b72641
User <User@100.74.245.73>
2016-11-19 21:34:27 +0000

    Claim Class

--- Project_Folder/UWE_ESD/src/java/user/Claim.java
+++ Project_Folder/UWE_ESD/src/java/user/Claim.java
@@ -2,29 +2,52 @@ package user;
 
 /**
  *
- * @author Jayson
+ * @author jayson
  */
-public class Claim
-{
-  private String m_name;
-
-  private String m_claimType;
-
-  private Long  m_claimNumber;
-  
-  User user = new User();
-  
-  public String getClaimType()
-  {
-    return m_claimType;
-  }
-
-  public Long getClaimNumber()
-  {
-    return m_claimNumber;
-  }
-}
-
-
+public class Claim {
+    private String status;
+    private String dor;
+    private int max_claim;
+    private double claim_amount;
+   
     
+    public Claim(){}
+    public Claim(String status,double balance,int max_claim){
+        
+    }
+    public Claim(String status, String dor,double claim_amount){
+        this.status=status;
+        this.dor=dor;
+        this.claim_amount=claim_amount;
+        this.max_claim=max_claim;
+    }
 
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDor() {
+        return dor;
+    }
+
+    public void setClaim_amount(double claim_amount) {
+        this.claim_amount=claim_amount;
+    }
+    
+    public double getClaim_amount() {
+        return claim_amount;
+    }
+
+    public void setMax_claim(int max_claim) {
+        this.max_claim=max_claim;
+    }
+    
+    public int getMax_claim() {
+        return max_claim;
+    }
+
+}
